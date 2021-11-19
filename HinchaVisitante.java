0f93d5ca7e84aa0d93aa23a14b287b5ec1c7b1a7
Juandie Derewicki <jdderewicki@gmail.com>
2021-11-19 09:11:52 -0300

    no message

---
+++ HinchaVisitante.java
@@ -0,0 +1,12 @@
+package TRABAJOFINALLAB;
+
+import java.io.Serializable;
+
+public class HinchaVisitante extends Persona implements Serializable {
+    public HinchaVisitante(Integer DNI, String nombre, String apellido) {
+        super(DNI, nombre, apellido);
+    }
+
+
+}
+
