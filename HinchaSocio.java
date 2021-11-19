35d2fbf763cedf7e56711911819dfea00b1889b5
Matias Guagni <85851958+Matiguagni@users.noreply.github.com>
2021-11-19 09:44:50 -0300

    Add files via upload

---
+++ HinchaSocio.java
@@ -0,0 +1,8 @@
+package TRABAJOFINALLAB;
+
+public class HinchaSocio extends Persona{
+    public HinchaSocio(Integer DNI, String nombre, String apellido) {
+        super(DNI, nombre, apellido);
+    }
+
+}
