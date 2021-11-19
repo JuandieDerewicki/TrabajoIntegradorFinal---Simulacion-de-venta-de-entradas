3eb411193e3e64e5522cbb04f27673cc8d42be63
Franco Rojas <89144104+francor5@users.noreply.github.com>
2021-11-19 09:59:57 -0300

    Add files via upload

---
+++ Hincha.java
@@ -0,0 +1,15 @@
+package TRABAJOFINALLAB;
+
+import java.io.Serializable;
+
+public class Hincha extends Persona implements Serializable {
+    public Hincha(Integer DNI, String nombre, String apellido) {
+        super(DNI, nombre, apellido);
+    }
+
+    public Hincha(HinchaSocio hincha1) {
+
+
+
+    }
+}
