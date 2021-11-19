dd82d1d8b786b13611fee8020690f85fb2d15b43
Matias Guagni <85851958+Matiguagni@users.noreply.github.com>
2021-11-19 10:42:42 -0300

    Delete HinchaSocio.java

--- HinchaSocio.java
+++
@@ -1,8 +0,0 @@
-package TRABAJOFINALLAB;
-
-public class HinchaSocio extends Persona{
-    public HinchaSocio(Integer DNI, String nombre, String apellido) {
-        super(DNI, nombre, apellido);
-    }
-
-}
