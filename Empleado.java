7d0d299070ebb550ed58d436a6e4eb52f7da0b3d
Maxrd02 <Mirotimimaxi@gmail.com>
2021-11-19 09:31:13 -0300

    no message

---
+++ Empleado.java
@@ -0,0 +1,9 @@
+package TRABAJOFINALLAB;
+
+public class Empleado extends Persona{
+    public Empleado(Integer DNI, String nombre, String apellido) {
+        super(DNI, nombre, apellido);
+    }
+
+}
+
